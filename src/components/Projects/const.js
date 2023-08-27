c70240357823db9767b5f5d98d3864015c5d64c4
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-08-27 13:36:57 +1000

    11: изменение стилей бургер-меню

--- src/components/Projects/const.js
+++ src/components/Projects/const.js
@@ -7,9 +7,9 @@ import cartPic from './../../assets/imgs/projects/cart.png';
 
 export const projectsConst = [
     {name: 'CRM-React', img: crmPic, link: 'https://nikitazhvalik.github.io/crm-build/#/table'},
-    {name: 'Portfolio-React', img: portfolioPic, link: 'https://nikitazhvalik.github.io/portfolio-react-build/'},
     {name: 'Blog-React', img: blogPic, link: 'https://nikitazhvalik.github.io/react-blog-build/'},
     {name: 'BuyHouse-JS', img: homePic, link: 'https://nikitazhvalik.github.io/house-to-buy-build/'},
     {name: 'Calculator-JS', img: calcPic, link: 'https://nikitazhvalik.github.io/cart-build/'},
     {name: 'Cart-React', img: cartPic, link: 'https://nikitazhvalik.github.io/calculator/'},
+    {name: 'Portfolio-React', img: portfolioPic, link: 'https://nikitazhvalik.github.io/portfolio-react-build/'},
 ]
