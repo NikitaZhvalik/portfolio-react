d882d86aec84458f49454825da257bb79d98fa31
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-09-08 16:31:02 +1000

    16: изм проекта + стилизация ссылок в бургере

--- src/components/Projects/const.js
+++ src/components/Projects/const.js
@@ -4,12 +4,13 @@ import homePic from './../../assets/imgs/projects/buyHouse.png';
 import portfolioPic from './../../assets/imgs/projects/portfolio.png';
 import calcPic from './../../assets/imgs/projects/calculator.png';
 import cartPic from './../../assets/imgs/projects/cart.png';
+import cardGame from './../../assets/imgs/projects/cardGame.png';
 
 export const projectsConst = [
-    {name: 'CRM-React', img: crmPic, link: 'https://nikitazhvalik.github.io/crm-build/#/table'},
-    {name: 'Blog-React', img: blogPic, link: 'https://nikitazhvalik.github.io/react-blog-build/'},
-    {name: 'BuyHouse-JS', img: homePic, link: 'https://nikitazhvalik.github.io/house-to-buy-build/'},
-    {name: 'Calculator-JS', img: calcPic, link: 'https://nikitazhvalik.github.io/cart-build/'},
-    {name: 'Cart-React', img: cartPic, link: 'https://nikitazhvalik.github.io/calculator/'},
-    {name: 'Portfolio-React', img: portfolioPic, link: 'https://nikitazhvalik.github.io/portfolio-react-build/'},
+    {name: 'CRM - React', img: crmPic, link: 'https://nikitazhvalik.github.io/crm-build/#/table'},
+    {name: 'Blog - React', img: blogPic, link: 'https://nikitazhvalik.github.io/react-blog-build/'},
+    {name: 'BuyHouse - JS', img: homePic, link: 'https://nikitazhvalik.github.io/house-to-buy-build/'},
+    {name: 'Card Game - TS', img: cardGame, link: 'https://nikitazhvalik.github.io/card-game-build/'},
+    {name: 'Calculator - JS', img: calcPic, link: 'https://nikitazhvalik.github.io/calculator/'},
+    {name: 'Cart - React', img: cartPic, link: 'https://nikitazhvalik.github.io/cart-build/'},
 ]
--- src/components/Skills/const.js
+++ src/components/Skills/const.js
@@ -8,16 +8,18 @@ import postmanPic from './../../assets/imgs/skills/postman.svg';
 import webpackPic from './../../assets/imgs/skills/webpack.svg';
 import vscodePic from './../../assets/imgs/skills/VS-code.svg';
 import githubPic from './../../assets/imgs/skills/github.svg';
+import sassPic from './../../assets/imgs/skills/sass.svg'
+import tsPic from './../../assets/imgs/skills/typescript.svg'
 
 export const skillsConst = [
     {name: 'HTML5', img: htmlPic},
     {name: 'CSS', img: cssPic},
+    {name: 'SASS', img: sassPic},
     {name: 'JS', img: jsPic},
+    {name: 'Webpack', img: webpackPic},
     {name: 'React', img: reactPic},
     {name: 'Redux', img: reduxPic},
-    {name: 'Node.js', img: nodejsPic},
+    {name: 'TypeScript', img: tsPic},
     {name: 'Postman', img: postmanPic},
-    {name: 'Webpack', img: webpackPic},
-    {name: 'VS code', img: vscodePic},
     {name: 'GitHub', img: githubPic},
 ]
